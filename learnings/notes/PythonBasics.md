39c8adcb7165d1c068359ec4a3cc8f956aca93c9
rukhaya <deep.aman91@gmail.com>
2018-09-01 00:08:24 +0530

    DS-0: python gyan

--- learnings/notes/PythonBasics.md
+++ learnings/notes/PythonBasics.md
@@ -1,3 +1,9 @@
-import sys<br>
+import sys
+
 print(sys.executable) # prints the path of python.exe
 
+conda update -n base -c defaults conda
+
+conda install -c conda-forge jupyterlab
+
+pip install jupyter_contrib_nbextensions
---
+++ learnings/notes/PythonGyanLessons/HelloWorld.py
@@ -0,0 +1,18 @@
+"""
+This module is always available.  It provides access to the
+mathematical functions defined by the C standard.
+"""
+
+
+my_first_variable = "Hello World"
+
+import math
+
+print(my_first_variable)
+
+def my_hello():
+    '''
+    It prints 'India is great'
+    :return: None
+    '''
+    print('India is great')
